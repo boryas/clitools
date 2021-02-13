d4c6ab475aabdcb302704fdaf996b40483a8a6df
Boris Burkov <boris@bur.io>
2021-02-13 18:11:33 +0000

    add del as C, list/tag as shell examples

---
+++ example/bote/del/del.c
@@ -0,0 +1,24 @@
+#include <errno.h>
+#include <error.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+static const char bote_home[] = "/home/bb/.bote/";
+
+int main(int argc, char *argv[]) {
+  int ret;
+  char *fname;
+
+  fname = malloc(strlen(bote_home) + strlen(argv[1]) + 1);
+  if (!fname) {
+    error(1, ENOMEM, "failed to allocate note filename\n");
+  }
+  fname = strcpy(fname, bote_home);
+  fname = strcat(fname, argv[1]);
+  ret = unlink(fname);
+  if (ret)
+    error(1, errno, "failed to unlink note %s\n", fname);
+  return 0;
+}
