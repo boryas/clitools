2328370f0e7b3e080b25100c0a9d3c14a6f80016
Boris Burkov <boris@bur.io>
2022-05-17 15:54:43 -0700

    use homedir instead of /usr/local and /etc

--- Makefile
+++ Makefile
@@ -1,5 +1,6 @@
 CC=gcc
 CC_OPTS="-ggdb"
+RCLI_DIR=$${HOME}/.rcli/clis
 
 all: rcli shm
 
@@ -10,12 +11,12 @@ shm: shm.c
 	$(CC) $(CC_OPTS) shm.c -o shm -lrt
 
 install: rcli
-	cp rcli /usr/local/bin
+	cp rcli $${HOME}/.local/bin/
 
 install_bote:
-	rm -rf /etc/rcli/bote
-	mkdir -p /etc/rcli/bote
-	cp -r example/bote/* /etc/rcli/bote
+	rm -rf ${RCLI_DIR}/bote
+	mkdir -p ${RCLI_DIR}/bote
+	cp -r example/bote/* ${RCLI_DIR}/bote
 
 clean:
 	rm -f rcli 2>/dev/null
--- rcli.c
+++ rcli.c
@@ -1,382 +1,422 @@
 #include <dirent.h>
 #include <errno.h>
+#include <error.h>
 #include <fcntl.h>
 #include <libgen.h>
+#include <pwd.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/stat.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
 
 #define RCLI_BUF_4K 4096
 
-static const char RCLI_DIR[] = "/etc/rcli/";
+static const char RCLI_PATH[] = "/.rcli/clis/";
 
 struct rcli {
-  char *path;
-  char *name;
-  size_t depth;
-  size_t sz;
-  struct rcli *sub_clis;
+	char *path;
+	char *name;
+	size_t depth;
+	size_t sz;
+	struct rcli *sub_clis;
 };
 
 static char *get_basename(char *path) {
-  char *cpy;
-  char *bn;
-  char *ret;
-  int path_len = strlen(path);
+	char *cpy;
+	char *bn;
+	char *ret;
+	int path_len = strlen(path);
 
-  cpy = malloc(path_len + 1);
-  if (!cpy) {
-    return NULL;
-  }
-  cpy = strcpy(cpy, path);
-  bn = basename(cpy);
-  ret = malloc(strlen(bn) + 1);
-  if (!ret) {
-    ret = NULL;
-    goto out;
-  }
-  ret = strcpy(ret, bn);
+	cpy = malloc(path_len + 1);
+	if (!cpy) {
+		return NULL;
+	}
+	cpy = strcpy(cpy, path);
+	bn = basename(cpy);
+	ret = malloc(strlen(bn) + 1);
+	if (!ret) {
+		ret = NULL;
+		goto out;
+	}
+	ret = strcpy(ret, bn);
 out:
-  free(cpy);
-  return ret;
+	free(cpy);
+	return ret;
 }
 
 int rcli_init(struct rcli *cli, char *path, size_t depth) {
-  char *cpy;
-  char *name;
-  int ret;
+	char *cpy;
+	char *name;
+	int ret;
 
-  if (!cli) {
-    ret = EINVAL;
-    goto out;
-  }
+	if (!cli) {
+		ret = -EINVAL;
+		goto out;
+	}
 
-  memset(cli, 0, sizeof(*cli));
+	memset(cli, 0, sizeof(*cli));
 
-  cli->path = malloc(strlen(path) + 2);
-  if (!cli->path) {
-    ret = ENOMEM;
-    goto out;
-  }
-  cli->path = strcpy(cli->path, path);
-  cli->path = strcat(cli->path, "/");
-  cli->name = get_basename(path);
-  if (!cli->name) {
-    goto free_path;
-  }
-  cli->depth = depth;
+	cli->path = malloc(strlen(path) + 2);
+	if (!cli->path) {
+		ret = -ENOMEM;
+		goto out;
+	}
+	cli->path = strcpy(cli->path, path);
+	cli->path = strcat(cli->path, "/");
+	cli->name = get_basename(path);
+	if (!cli->name) {
+		goto free_path;
+	}
+	cli->depth = depth;
 
-  ret = 0;
-  goto out;
+	ret = 0;
+	goto out;
 
 free_path:
-  free(cli->path);
+	free(cli->path);
 out:
-  return ret;
+	return ret;
 }
 
 void rcli_free(struct rcli *cli) {
-  int i;
+	int i;
 
-  if (cli->path) {
-    free(cli->path);
-  }
-  if (cli->name) {
-    free(cli->name);
-  }
-  for (i = 0; i < cli->sz; ++i) {
-    rcli_free(&cli->sub_clis[i]);
-  }
+	if (cli->path) {
+		free(cli->path);
+	}
+	if (cli->name) {
+		free(cli->name);
+	}
+	for (i = 0; i < cli->sz; ++i) {
+		rcli_free(&cli->sub_clis[i]);
+	}
 }
 
 int rcli_traverse(struct rcli *cli, int (*dir_op)(struct rcli*, struct dirent*, int i)) {
-  int ret = 0;
-  DIR *dir;
-  struct dirent *dent;
-  int i = 0;
+	int ret = 0;
+	DIR *dir;
+	struct dirent *dent;
+	int i = 0;
 
-  dir = opendir(cli->path);
-  if (!dir) {
-    return errno;
-  }
-  while (true) {
-    errno = 0;
-    dent = readdir(dir);
-    if (!dent) {
-      if (errno) {
-        ret = errno;
-        goto out;
-      }
-      break;
-    }
-    if (dent->d_type == DT_DIR) {
-      if (!strcmp(dent->d_name, "..") || !strcmp(dent->d_name, ".")) {
-        continue;
-      }
-      ret = dir_op(cli, dent, i);
-      if (ret) {
-        goto out;
-      }
-      ++i;
-    }
-  }
+	dir = opendir(cli->path);
+	if (!dir) {
+		return errno;
+	}
+	while (true) {
+		errno = 0;
+		dent = readdir(dir);
+		if (!dent) {
+			if (errno) {
+				ret = errno;
+				goto out;
+			}
+			break;
+		}
+		if (dent->d_type == DT_DIR) {
+			if (!strcmp(dent->d_name, "..") || !strcmp(dent->d_name, ".")) {
+				continue;
+			}
+			ret = dir_op(cli, dent, i);
+			if (ret) {
+				goto out;
+			}
+			++i;
+		}
+	}
 out:
-  closedir(dir);
-  return ret;
+	closedir(dir);
+	return ret;
 }
 
 int rcli_op_inc_sz(struct rcli *cli, struct dirent *dent, int i) {
-  ++cli->sz;
-  return 0;
+	++cli->sz;
+	return 0;
 }
 
 int rcli_op_add_subcli(struct rcli *cli, struct dirent *dent, int i) {
-  int ret;
-  char *path = malloc(strlen(cli->path) + strlen(dent->d_name) + 1);
+	int ret;
+	char *path = malloc(strlen(cli->path) + strlen(dent->d_name) + 1);
 
-  if (!path) {
-    return ENOMEM;
-  }
-  path = strcpy(path, cli->path);
-  path = strcat(path, dent->d_name);
+	if (!path) {
+		return -ENOMEM;
+	}
+	path = strcpy(path, cli->path);
+	path = strcat(path, dent->d_name);
 
-  ret = rcli_init(&cli->sub_clis[i], path, cli->depth + 1);
+	ret = rcli_init(&cli->sub_clis[i], path, cli->depth + 1);
 
-  free(path);
-  return ret;
+	free(path);
+	return ret;
 }
 
 static int count_sub_clis(struct rcli *cli) {
-  return rcli_traverse(cli, rcli_op_inc_sz);
+	return rcli_traverse(cli, rcli_op_inc_sz);
 }
 
 static int get_sub_clis(struct rcli *cli) {
-  return rcli_traverse(cli, rcli_op_add_subcli);
+	return rcli_traverse(cli, rcli_op_add_subcli);
 }
 
 int rcli_populate(struct rcli *cli) {
-  int ret;
-  int i = 0;
+	int ret;
+	int i = 0;
 
-  ret = count_sub_clis(cli);
-  if (ret < 0) {
-    return ret;
-  }
-  cli->sub_clis = calloc(cli->sz, sizeof(struct rcli));
-  if (!cli->sub_clis) {
-    return ENOMEM;
-  }
+	ret = count_sub_clis(cli);
+	if (ret < 0) {
+		return ret;
+	}
+	cli->sub_clis = calloc(cli->sz, sizeof(struct rcli));
+	if (!cli->sub_clis) {
+		return -ENOMEM;
+	}
 
-  ret = get_sub_clis(cli);
-  if (ret < 0) {
-    return ret;
-  }
+	ret = get_sub_clis(cli);
+	if (ret < 0) {
+		return ret;
+	}
 
-  for (i = 0; i < cli->sz; ++i) {
-    ret = rcli_populate(&cli->sub_clis[i]);
-    if (ret < 0) {
-      return ret;
-    }
-  }
-  return 0;
+	for (i = 0; i < cli->sz; ++i) {
+		ret = rcli_populate(&cli->sub_clis[i]);
+		if (ret < 0) {
+			return ret;
+		}
+	}
+	return 0;
 }
 
 struct rcli *rcli_find_subcli(struct rcli *cli, int argc, char *argv[]) {
-  struct rcli *cur = cli;
+	struct rcli *cur = cli;
 
-  for (int i = optind; i < argc; ++i) {
-    for (int j = 0; j < cur->sz; ++j) {
-      if (!strcmp(cur->sub_clis[j].name, argv[i])) {
-        cur = &cur->sub_clis[j];
-        break;
-      }
-    }
-  }
-  return cur;
+	for (int i = optind; i < argc; ++i) {
+		for (int j = 0; j < cur->sz; ++j) {
+			if (!strcmp(cur->sub_clis[j].name, argv[i])) {
+				cur = &cur->sub_clis[j];
+				break;
+			}
+		}
+	}
+	return cur;
 }
 
 static ssize_t rcli_stream(int fdin, int fdout) {
-  char buf[RCLI_BUF_4K];
-  ssize_t sz;
-  ssize_t streamed;
+	char buf[RCLI_BUF_4K];
+	ssize_t sz;
+	ssize_t streamed;
 
-  while ((sz = read(fdin, buf, RCLI_BUF_4K))) {
-    if (sz < 0) {
-      if (errno == EINTR)
-        continue;
-      // TODO return stream read error
-      fprintf(stderr, "failed to read %d: %d\n", fdin, errno);
-      return -errno;
-    }
-    while (sz) {
-      ssize_t w = write(fdout, buf, sz);
-      if (w < 0) {
-        if (errno == EINTR)
-          continue;
-        // TODO return stream write error
-        fprintf(stderr, "failed to write to %d: %d\n", fdout, errno);
-        return -errno;
-      }
-      streamed += w;
-      sz -= w;
-    }
-  }
-  return streamed;
+	while ((sz = read(fdin, buf, RCLI_BUF_4K))) {
+		if (sz < 0) {
+			if (errno == EINTR)
+				continue;
+			// TODO return stream read error
+			error(0, errno, "failed to read from %d", fdin);
+			return -errno;
+		}
+		while (sz) {
+			ssize_t w = write(fdout, buf, sz);
+			if (w < 0) {
+				if (errno == EINTR)
+					continue;
+				// TODO return stream write error
+				error(0, errno, "failed to write to %d", fdin);
+				return -errno;
+			}
+			streamed += w;
+			sz -= w;
+		}
+	}
+	return streamed;
 }
 
 static char *rcli_file_path(struct rcli *cli, char *fname) {
-  char *path;
+	char *path;
 
-  path = malloc(strlen(cli->path) + strlen(fname) + 1);
-  if (!path) {
-    return NULL;
-  }
-  path = strcpy(path, cli->path);
-  path = strcat(path, fname);
-  return path;
+	path = malloc(strlen(cli->path) + strlen(fname) + 1);
+	if (!path) {
+		return NULL;
+	}
+	path = strcpy(path, cli->path);
+	path = strcat(path, fname);
+	return path;
 }
 
-static int rcli_dump(struct rcli *cli, char *fname) {
-  int fd;
-  ssize_t ret;
-  char *path;
+static int rcli_dump(int out_fd, struct rcli *cli, char *fname) {
+	int fd;
+	ssize_t ret;
+	char *path;
 
-  path = rcli_file_path(cli, fname);
-  if (!path) {
-    fprintf(stderr, "failed to allocate rcli filename %s: %d\n", fname, errno);
-    return -ENOMEM;
-  }
+	path = rcli_file_path(cli, fname);
+	if (!path) {
+		error(0, ENOMEM, "failed to allocate rcli filename %s", fname);
+		return -ENOMEM;
+	}
 
-  fd = open(path, O_RDONLY);
-  if (fd < 0) {
-    fprintf(stderr, "failed to open %s for dump: %d\n", path, errno);
-    ret = -errno;
-    goto out;
-  }
+	fd = open(path, O_RDONLY);
+	if (fd < 0) {
+		error(0, errno, "failed to open %s for dump", path);
+		ret = -errno;
+		goto out;
+	}
 
-  ret = rcli_stream(fd, STDOUT_FILENO);
-  if (ret < 0)
-    fprintf(stderr, "failed to dump %s: %d\n", path, errno);
+	ret = rcli_stream(fd, out_fd);
+	if (ret < 0)
+		error(0, errno, "failed to dump %s", path);
 
 out:
-  close(fd);
-  free(path);
-  return ret;
+	close(fd);
+	free(path);
+	return ret;
 }
 
 static int rcli_do_help(struct rcli *cli) {
-  return rcli_dump(cli, "help");
+	return rcli_dump(STDOUT_FILENO, cli, "help");
 }
 
 static int rcli_do_usage(struct rcli *cli) {
-  printf("\nUsage:\n");
-  return rcli_dump(cli, "usage");
+	fprintf(stderr, "\nUsage:\n");
+	return rcli_dump(STDERR_FILENO, cli, "usage");
 }
 
 /*
- * N.B. this ends with exec, so typically it won't return
+ * N.B. this ends with exec, so it won't return, except in
+ * error conditions.
  */
 static int rcli_do_exec(struct rcli *cli, char **argv) {
-  char *run_f;
+	char *run_f;
+	struct stat st;
+	int st_ret;
 
-  run_f = malloc(strlen(cli->path) + strlen("run") + 1);
-  if (!run_f) {
-    fprintf(stderr, "failed to allocate rcli run filename: %d\n", errno);
-    return ENOMEM;
-  }
-  run_f = strcpy(run_f, cli->path);
-  run_f = strcat(run_f, "run");
+	run_f = malloc(strlen(cli->path) + strlen("run") + 1);
+	if (!run_f) {
+		error(0, ENOMEM, "failed to allocate rcli run filename");
+		return -ENOMEM;
+	}
+	run_f = strcpy(run_f, cli->path);
+	run_f = strcat(run_f, "run");
 
-  execvp(run_f, argv);
-  fprintf(stderr, "failed to execvp %s: %d\n", run_f, errno);
-  return errno;
+	st_ret = stat(run_f, &st);
+	if (st_ret) {
+		error(0, errno, "failed to find run file for cli at %s", cli->path);
+		return -errno;
+	}
+
+	execvp(run_f, argv);
+	error(0, errno, "failed to execvp %s", run_f);
+	return -errno;
 }
 
 int rcli_run_cli(struct rcli *cli, int argc, char *argv[]) {
-  int c;
-  bool help = false;
-  bool verbose = false;
-  struct rcli *sub_cli;
-  int ret;
-  int status;
+	int c;
+	bool help = false;
+	bool verbose = false;
+	struct rcli *sub_cli;
+	int ret;
+	int status;
 
-  // handle universal options
-  // ignore unknown
-  // get optind to the sub-command
-  // long opts?
-  // usage
-  while ((c = getopt(argc, argv, "hv")) != -1) {
-    switch (c) {
-      case 'h':
-        help = true;
-        break;
-      case 'v':
-        verbose = true;
-        break;
-      case '?':
-        fprintf(stderr, "unknown option '-%c'", optopt);
-        break;
-      default:
-        return -1;
-    }
-  }
-  // walk rcli to find subcommand
-  sub_cli = rcli_find_subcli(cli, argc, argv);
+	// handle universal options
+	// ignore unknown
+	// get optind to the sub-command
+	// long opts?
+	// usage
+	while ((c = getopt(argc, argv, "hv")) != -1) {
+		switch (c) {
+		case 'h':
+			help = true;
+			break;
+		case 'v':
+			verbose = true;
+			break;
+		case '?':
+			error(0, 0, "unknown option '-%c'", optopt);
+			break;
+		default:
+			return -1;
+		}
+	}
+	// walk rcli to find subcommand
+	sub_cli = rcli_find_subcli(cli, argc, argv);
 
-  argc -= sub_cli->depth;
-  argv += sub_cli->depth;
+	argc -= sub_cli->depth;
+	argv += sub_cli->depth;
 
-  if (help)
-    return rcli_do_help(sub_cli);
+	if (help)
+		return rcli_do_help(sub_cli);
 
-  ret = fork();
-  if (ret < 0)
-    return ret;
-  if (!ret) {
-    // child runs the cli
-    ret = rcli_do_exec(sub_cli, argv + optind);
-    // Don't expect this to actually execute. ^ execs
-    return ret;
-  } else {
-    // parent waits, in case of EINVAL (?)
-    ret = waitpid(ret, &status, 0);
-    if (WEXITSTATUS(status) == EINVAL)
-      return rcli_do_usage(sub_cli);
-  }
+	ret = fork();
+	if (ret < 0)
+		return ret;
+	if (!ret) {
+		// child runs the cli
+		ret = rcli_do_exec(sub_cli, argv + optind);
+		// Don't expect this to actually execute. ^ execs
+		return ret;
+	} else {
+		// parent waits, in case of EINVAL (?)
+		ret = waitpid(ret, &status, 0);
+		if (WEXITSTATUS(status) == EINVAL)
+			return rcli_do_usage(sub_cli);
+	}
+}
+
+static int get_rcli_dir(char **dir) {
+	uid_t euid;
+	struct passwd *pw;
+	size_t len;
+
+	euid = geteuid();
+again:
+	pw = getpwuid(euid);
+	if (!pw) {
+		if (errno == EINTR)
+			goto again;
+		return -errno;
+	}
+
+	len = strlen("/home/") + strlen(pw->pw_name) + strlen(RCLI_PATH);
+	*dir = (char *)malloc(len + 1);
+	if (!*dir)
+		return -ENOMEM;
+	strcpy(*dir, "/home/");
+	strcat(*dir, pw->pw_name);
+	strcat(*dir, RCLI_PATH);
+	return len;
 }
 
 int main(int argc, char *argv[]) {
-  char *cmd_path;
-  struct rcli cli;
-  int ret = 0;
+	char *cmd_path;
+	char *rcli_dir = NULL;
+	struct rcli cli;
+	int ret = 0;
 
-  if (argc < 2) {
-    printf("usage: rcli <CLI-DIR> [command]\n");
-    exit(1);
-  }
+	if (argc < 2) {
+		fprintf(stderr, "usage: rcli <CLI> [command]\n");
+		exit(1);
+	}
+	
+	ret = get_rcli_dir(&rcli_dir);
+	if (ret < 0)
+		error(1, 0, "unable to get rcli dir\n");
 
-  cmd_path = malloc(strlen(RCLI_DIR) + strlen(argv[1]) + 1);
-  if (!cmd_path) {
-    fprintf(stderr, "failed to allocate cli base path name\n");
-    exit(1);
-  }
+	cmd_path = malloc(ret + strlen(argv[1]) + 1);
+	if (!cmd_path)
+		error(1, ENOMEM, "failed to allocate cli base path name\n");
 
-  cmd_path = strcpy(cmd_path, RCLI_DIR);
-  cmd_path = strcat(cmd_path, argv[1]);
+	cmd_path = strcpy(cmd_path, rcli_dir);
+	free(rcli_dir);
+	cmd_path = strcat(cmd_path, argv[1]);
 
-  ret = rcli_init(&cli, cmd_path, 0);
-  if (ret) {
-    exit(ret);
-  }
+	ret = rcli_init(&cli, cmd_path, 0);
+	if (ret) {
+		exit(ret);
+	}
 
-  ret = rcli_populate(&cli);
-  if (ret) {
-    exit(ret);
-  }
+	ret = rcli_populate(&cli);
+	if (ret) {
+		exit(ret);
+	}
 
-  return rcli_run_cli(&cli, argc, argv);
+	return rcli_run_cli(&cli, argc, argv);
 }
